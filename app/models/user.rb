5421852d6909648786e1d63badff97429b8ee7e7
iamrubydevi <ariesabyss@gmail.com>
2023-09-29 12:03:10 +0530

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def recent_posts
-    posts.order(created_at: :desc).limit(3)
-  end
-
   # Validation for name
   validates :name, presence: true
 
   # Validation for post_counter
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
