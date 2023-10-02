0447801eb521d448d11b8c52fa83cdcc8c8f1fbe
iamrubydevi <ariesabyss@gmail.com>
2023-10-02 18:22:34 +0530

    Organize more semantically by grouping based on actions

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,37 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  context 'GET /users/123/posts' do
-    it 'renders a successful response' do
-      get user_posts_path(123)
-      expect(response).to have_http_status(:success)
-    end
+  describe 'GET /users/123/posts' do
+    context 'index action' do
+      it 'renders a successful response' do
+        get user_posts_path(123)
+        expect(response).to have_http_status(:success)
+      end
 
-    it 'renders the index template' do
-      get user_posts_path(123)
-      expect(response).to render_template(:index)
-    end
+      it 'renders the index template' do
+        get user_posts_path(123)
+        expect(response).to render_template(:index)
+      end
 
-    it 'includes correct placeholder text in the response body' do
-      get user_posts_path(123)
-      expect(response.body).to include('Here is a list of posts for a given user')
+      it 'includes correct placeholder text in the response body' do
+        get user_posts_path(123)
+        expect(response.body).to include('Here is a list of posts for a given user')
+      end
     end
   end
 
-  context 'GET /users/123/posts/1' do
-    it 'renders a successful response' do
-      get user_post_path(123, 1)
-      expect(response).to have_http_status(:success)
-    end
+  describe 'GET /users/123/posts/1' do
+    context 'show action' do
+      it 'renders a successful response' do
+        get user_post_path(123, 1)
+        expect(response).to have_http_status(:success)
+      end
 
-    it 'renders the show template' do
-      get user_post_path(123, 1)
-      expect(response).to render_template(:show)
-    end
+      it 'renders the show template' do
+        get user_post_path(123, 1)
+        expect(response).to render_template(:show)
+      end
 
-    it 'includes correct placeholder text in the response body' do
-      get user_post_path(123, 1)
-      expect(response.body).to include('Here is a selected post from posts')
+      it 'includes correct placeholder text in the response body' do
+        get user_post_path(123, 1)
+        expect(response.body).to include('Here is a selected post from posts')
+      end
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,37 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  context 'GET /users' do
-    it 'renders a successful response' do
-      get users_path
-      expect(response).to have_http_status(:success)
-    end
+  describe 'GET /users' do
+    context 'index action' do
+      it 'renders a successful response' do
+        get users_path
+        expect(response).to have_http_status(:success)
+      end
 
-    it 'renders the index template' do
-      get users_path
-      expect(response).to render_template(:index)
-    end
+      it 'renders the index template' do
+        get users_path
+        expect(response).to render_template(:index)
+      end
 
-    it 'includes correct placeholder text in the response body' do
-      get users_path
-      expect(response.body).to include('Here is a list of users')
+      it 'includes correct placeholder text in the response body' do
+        get users_path
+        expect(response.body).to include('Here is a list of users')
+      end
     end
   end
 
-  context 'GET /users/123' do
-    it 'renders a successful response' do
-      get user_path(123)
-      expect(response).to have_http_status(:success)
-    end
+  describe 'GET /users/123' do
+    context 'show action' do
+      it 'renders a successful response' do
+        get user_path(123)
+        expect(response).to have_http_status(:success)
+      end
 
-    it 'renders the show template' do
-      get user_path(123)
-      expect(response).to render_template(:show)
-    end
+      it 'renders the show template' do
+        get user_path(123)
+        expect(response).to render_template(:show)
+      end
 
-    it 'includes correct placeholder text in the response body' do
-      get user_path(123)
-      expect(response.body).to include('Here is a selected user from users list')
+      it 'includes correct placeholder text in the response body' do
+        get user_path(123)
+        expect(response.body).to include('Here is a selected user from users list')
+      end
     end
   end
 end
