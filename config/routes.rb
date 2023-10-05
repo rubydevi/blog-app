0fe9cbbe9f14f117a0aef429eca2a980d3cfc8aa
iamrubydevi <ariesabyss@gmail.com>
2023-10-05 14:30:51 +0530

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:new, :create]
     end
