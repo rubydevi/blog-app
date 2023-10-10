43f6ecdd8b58b9a678197ca3db1a6417af8b603b
iamrubydevi <ariesabyss@gmail.com>
2023-10-10 22:35:56 +0530

    Update user authentication and post_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,12 @@ class User < ApplicationRecord
   # Validation for post_counter
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.post_counter ||= 0
+  end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
