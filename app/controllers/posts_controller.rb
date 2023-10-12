7e243f62bb1d0110e52a279e70e866f286b7dede
Diana Beki <dianabeki54@gmail.com>
2023-10-12 18:01:26 +0300

    Update API endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,57 +1,9 @@
-class PostsController < ApplicationController
-  load_and_authorize_resource
-  before_action :set_current_user, only: %i[new create destroy]
+def index
+  @user = User.includes(posts: :comments).find(params[:user_id])
+  @posts = @user.posts.includes(:comments)
 
-  def index
-    @user = User.includes(posts: :comments).find(params[:user_id])
-    @posts = @user.posts.includes(:comments)
-  end
-
-  def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.includes(:comments).find(params[:id])
-  end
-
-  def new
-    @post = @user.posts.build
-  end
-
-  def create
-    @post = Post.new(
-      author: @user,
-      title: params[:post][:title],
-      text: params[:post][:text],
-      comments_counter: 0,
-      likes_counter: 0
-    )
-
-    if @post.save
-      flash.now[:error] = 'Post was successfully created.'
-      redirect_to user_posts_path(@user)
-    else
-      flash.now[:error] = 'Oops, something went wrong'
-      render :new
-    end
-  end
-
-  def destroy
-    @post = @user.posts.find(params[:id])
-
-    if @post.destroy
-      flash.now[:success] = "Your post titled: '#{@post.title}' was successfully deleted"
-    else
-      flash.now[:error] = 'Oops! Cannot delete your post.'
-    end
-    redirect_to user_posts_path(@user)
-  end
-
-  private
-
-  def set_current_user
-    @user = current_user
-  end
-
-  def post_params
-    params.require(:post).permit(:title, :text)
+  respond_to do |format|
+    format.html { render 'index' }
+    format.json { render json: @posts }
   end
 end
